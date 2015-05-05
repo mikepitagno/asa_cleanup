69cfa59f7a369162cd9ca122029e1b02a46b9d33
mikepitagno <mike.pitagno@gmail.com>
2015-05-05 15:53:53 -0400

    Added ACL cleanup support.

--- asa_cleanup.py
+++ asa_cleanup.py
@@ -6,19 +6,24 @@ import sys
 def create_list(config_file):
   objects = []
   object_groups = []
+  acls = []
   for line in config_file:
     if 'object network ' in line:
       object = (line.split()).pop(2)
       if not object in objects:
         objects.append(object)
     if 'object-group network ' in line:
-      object = (line.split()).pop(2)
-      if not object in object_groups:
-        object_groups.append(object)
-  return (objects, object_groups)
+      object_group = (line.split()).pop(2)
+      if not object_group in object_groups:
+        object_groups.append(object_group)
+    if line.startswith('access-list'):
+      acl = (line.split()).pop(1)
+      if not acl in acls:
+        acls.append(acl)
+  return (objects, object_groups, acls)
 
-# Create object dictionary; Select each object from list and search each line in config for that object; If key(object) not found in dictionary populate dictionary with object and assign value of 1; If key is found, increment value by one 
-def create_count(list, config_file):
+# Create object or object-group dictionary; Select each object from list and search each line in config for that object; If key(object) not found in dictionary populate dictionary with object and assign value of 1; If key is found, increment value by one 
+def create_object_count(list, config_file):
   count = {}
   for i in list:
     for line in config_file:
@@ -28,7 +33,24 @@ def create_count(list, config_file):
         else:
           count[i] = count[i] + 1
   return count
-  
+
+# Create acl dictionary; Select each acl from list and search each line in config for that object; If key(object) not found in dictionary populate dictionary with object and assign value of 1; If key is found, increment value by one as long as line does not begin with 'access-list'
+def create_acl_count(list, config_file):
+  count = {}
+  for i in list:
+    for line in config_file:
+      if line.startswith('access-list'):
+        if not i in count:
+          count[i] = 1
+        else:
+          continue
+      elif i in line:
+        if not i in count:
+          count[i] = 1
+        else:
+          count[i] = count[i] + 1
+  return count
+
 # Iterate through object dictionary and print keys that have a value of 1 (i.e. are not used anywhere in the config)
 def create_conf(dict, type):
   for i in dict.keys():
@@ -36,7 +58,9 @@ def create_conf(dict, type):
       if type == 'o':
         print "no object network %s" % (i)
       elif type == 'og':
-        print "no object-group network %s" % (i)        
+        print "no object-group network %s" % (i)
+      elif type == 'acl':
+        print "clear configure access-list %s" % (i)        
 
 # Main Program
 def main():
@@ -45,17 +69,28 @@ def main():
     config_file = f.readlines()	
     f.close()
   
-    objects, object_groups = create_list(config_file)
+    objects, object_groups, acls = create_list(config_file)
     
-    object_count = create_count(objects, config_file)
-    object_group_count = create_count(object_groups, config_file)
-  
-    print "\n"
-    print "Object Removal Lines:"
-    create_conf(object_count, 'o')
+    object_count = create_object_count(objects, config_file)
+    object_group_count = create_object_count(object_groups, config_file)
+    acl_count = create_acl_count(acls, config_file)
+
+#    print acl_count
+#    print "\n"
+#    print object_count
+#    print "\n"
+#    print object_group_count
+#    print "\n"
+    
+    print "ACL Removal Lines:"
+    create_conf(acl_count, 'acl')  
     print "\n"
     print "Object-Group Removal Lines:"
     create_conf(object_group_count, 'og')
+    print "\n"
+    print "Object Removal Lines:"
+    create_conf(object_count, 'o')
+    
   else:
     print "Config file missing.  Please include the full path of the ASA config file after the script."  
 
